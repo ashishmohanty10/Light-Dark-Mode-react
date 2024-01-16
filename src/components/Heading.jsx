076d6304d78a2fa09ae0e76917ccb2fadffd8ad7
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-16 19:18:08 +0530

    commit

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 const Heading = () => {
-  return <div></div>;
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <h1>Ligh-Dark-Mode</h1>
+
+          <button onClick={toggleTheme}>
+            <FontAwesomeIcon icon={darkTheme ? faSun : faMoon} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Heading;
