51f04154971d767bf737c8597acbe0cd24cbcae8
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-16 19:29:28 +0530

    implemented-dark-mode

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -9,14 +9,53 @@ const Heading = () => {
   };
 
   return (
-    <div>
-      <div className="container">
-        <div>
-          <h1>Ligh-Dark-Mode</h1>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="container dark:bg-[#2D3250]">
+        <div className="flex justify-center space-x-10 py-5">
+          <h1 className="text-4xl font-bold dark:text-slate-200">
+            Ligh-Dark-Mode
+          </h1>
 
           <button onClick={toggleTheme}>
-            <FontAwesomeIcon icon={darkTheme ? faSun : faMoon} />
+            <FontAwesomeIcon
+              size="2x"
+              icon={darkTheme ? faSun : faMoon}
+              color={darkTheme ? "#FFA500" : "#F0C420"}
+            />
           </button>
+        </div>
+
+        <div>
+          <p className="font-medium dark:text-slate-300">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+            facere eos cumque blanditiis saepe odit, iusto quo tempore velit
+            veniam corporis. Amet qui incidunt suscipit accusamus veniam
+            explicabo dolorum placeat laboriosam modi voluptas? At magni
+            suscipit dolorem alias delectus neque, optio temporibus, autem quae
+            maxime sapiente? Cum vitae, architecto minus eius fuga nobis libero
+            sit. Suscipit magni atque voluptatibus totam eligendi explicabo,
+            vero consequatur ex optio voluptatem veniam esse iste dicta dolorum
+            nemo hic harum adipisci itaque voluptatum. Modi odit aperiam debitis
+            ullam, quia quae, quis rem quas tempora sint sequi, eveniet ut illum
+            perspiciatis! Iste eveniet odit quam? Aspernatur.
+          </p>
+        </div>
+
+        <div className="py-3">
+          <p className="font-medium dark:text-slate-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ea
+            dolorem ad nesciunt recusandae. Iure similique, quos dignissimos
+            harum voluptas repellendus eligendi necessitatibus qui laudantium
+            doloremque consequuntur veritatis sint modi, corporis illum adipisci
+            error esse possimus ullam amet minus itaque quis labore. Ad harum
+            optio ullam obcaecati necessitatibus maxime ipsam libero totam
+            eligendi quidem omnis officia labore quod vitae iusto, corporis fuga
+            delectus dolores? Sit ipsam voluptate tempora debitis sed doloremque
+            esse at. Veniam dicta, quam, velit cum quos nisi placeat odit
+            nesciunt natus minima laboriosam odio, ea impedit rerum aut!
+            Similique recusandae nihil expedita. Asperiores est dolorum eaque
+            itaque!
+          </p>
         </div>
       </div>
     </div>
