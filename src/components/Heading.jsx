d1c7a14b945845066dc2339cde8414e802f9bea8
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-16 16:48:19 +0530

    returying with icons

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,59 +1,16 @@
 import React, { useState } from "react";
-import Sun from "../assets/sun.svg";
-import Moon from "../assets/moon.svg";
+import { FaSun, FaMoon } from "react-icons/fa6";
 const Heading = () => {
   const [darkTheme, setDarkTheme] = useState(false);
-
-  const toggleTheme = () => {
-    setDarkTheme(!darkTheme);
-  };
-
   return (
-    <div className={darkTheme ? "dark" : ""}>
-      <div className="container dark:bg-black h-screen">
-        <button className="dark:text-white" onClick={toggleTheme}>
-          CLick Me
-        </button>
-
-        <div className="flex justify-center ">
-          <h1 className="text-center py-5 dark:text-white">
-            Light & Dark Mode Practice
-          </h1>
+    <div>
+      <div className="container">
+        <div className="flex justify-center space-x-4 items-center py-5">
+          <h1>Dark-Light-Mode</h1>
+          <FaSun size={24} />
         </div>
 
-        <div className="dark:text-white">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-            quae nam minima ab officia. Fuga iure possimus saepe, qui debitis
-            commodi, sed quod praesentium reprehenderit nihil illo iste natus
-            rerum deleniti officiis quas omnis! Fuga, commodi! Dolor cupiditate
-            dolore ipsa reiciendis nulla quis neque delectus, quod vitae
-            accusantium blanditiis aut.
-          </p>
-
-          <p className="py-3">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum
-            similique aspernatur voluptatem soluta blanditiis temporibus unde
-            nostrum fugit quisquam sint, natus consectetur. Distinctio unde aut,
-            libero at veritatis iste totam, voluptates mollitia architecto
-            quaerat officia atque asperiores rerum ab modi. Assumenda eaque
-            fugit praesentium accusamus accusantium libero blanditiis doloribus
-            labore maiores reprehenderit laudantium maxime numquam, ducimus at
-            ipsam asperiores a, itaque aperiam aspernatur eos perferendis.
-            Voluptatem commodi sapiente impedit! Mollitia sit asperiores eius
-            minima neque illum error fugiat recusandae ducimus sapiente
-            veritatis, officia officiis dolore iusto hic doloremque tenetur,
-            praesentium magni illo totam, perferendis doloribus omnis. Vitae
-            nulla voluptates ex?
-          </p>
-        </div>
-
-        <div>
-          <form action="">
-            <input type="text" name="name" placeholder="name" />
-            <input type="email" name="email" placeholder="email" />
-          </form>
-        </div>
+        <div></div>
       </div>
     </div>
   );
